d1a16b9cba78dc17f5994367fb57d608234a6197
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-30 23:18:45 +0300

    1. Let's make some classrooms (automated command)

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
+export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
-
-module.exports = initializeRooms;
