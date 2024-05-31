e721ea78fd7dd63a192748571ab6d9e761803f9d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-31 22:06:50 +0300

    8. Primitive - Holberton Class (automated command)

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  toString() {
+    return this.location;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+}
