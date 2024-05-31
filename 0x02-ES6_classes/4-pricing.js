a9dd6e7bae5f16a85930b22fefb83e0a5b8b8978
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-31 03:28:38 +0300

    4. Pricing (automated command)

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+}
+
+module.exports = Pricing;
