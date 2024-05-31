ef1a79e1ccf1eca9627c46a8f89cc88dc38c2ed0
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-31 03:00:43 +0300

    (automated command)

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,18 +1,41 @@
 #!/usr/bin/node
 class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    this._name = name;
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
+  }
 
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
     if (typeof students !== 'object') {
       throw new TypeError('students must be a list');
     }
     this._students = students;
+  }
+
+  get students() {
+    return this._students;
   }
 }
 module.exports = HolbertonCourse;
