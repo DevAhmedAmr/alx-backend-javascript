aa195cab1cb220c22b162f9d14823a5c816fd941
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-31 03:14:29 +0300

    3. Methods, static methods, computed methods names..... MONEY  (automated command)

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+module.exports = Currency;
