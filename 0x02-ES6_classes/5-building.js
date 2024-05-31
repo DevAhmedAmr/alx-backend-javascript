1d0012740c737f1e59555a0ce2051fed6abe53cc
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-31 04:01:57 +0300

    4. Pricing (automated command)

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -19,11 +19,5 @@ class Building {
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage'
-    );
-  }
 }
 module.exports = Building;
