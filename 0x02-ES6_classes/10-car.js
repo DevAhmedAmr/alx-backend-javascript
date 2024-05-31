a5de4a15bcf01c4d7e9f7d2776aac69545b67980
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-31 23:38:23 +0300

    9. Hoisting (automated command)

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this.brand, this.motor, this.color);
+  }
+}
