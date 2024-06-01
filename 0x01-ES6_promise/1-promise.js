4175b3406d0911691143af21ea1bc596126eefc1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-01 23:06:37 +0300

    Update status value to be a number in getFullResponseFromAPI function
    Changed the status value from a string to a number in the resolve object
    to ensure consistency and accuracy in the getFullResponseFromAPI function.

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({ status: '200', body: 'Success' });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(Error('The fake API is not working currently'));
     }
